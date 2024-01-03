a53651929c5e50a6131438f10d5f4e6e79dcaf0e
mahdichi <mahdichi@gmail.com>
2024-01-03 10:43:39 +0330

    add ArmbianCompileDesktop

--- armbina_compile.sh
+++ armbina_compile.sh
@@ -34,13 +34,45 @@ ArmbianCompileServer()
 	#usbmount: ebhelper build-essential ntfs-3g fakeroot lockfile-progs
 	#docker:   libip6tc2 libnftnl11 iptables
 	#fula: logrotate
-} # ArmbianCompile
+
+} # ArmbianCompileServer
+########################################################
+ArmbianCompileDesktop()
+{
+	$ARMBIAN_BUILD_PATH/compile.sh \
+	BOARD=fxblox-rk1 \
+	BRANCH=legacy \
+	BUILD_DESKTOP=yes \
+	BUILD_MINIMAL=no \
+	DESKTOP_APPGROUPS_SELECTED='3dsupport browsers chat desktop_tools editors email internet multimedia office remote_desktop' \
+	DESKTOP_ENVIRONMENT=gnome \
+	DESKTOP_ENVIRONMENT_CONFIG_NAME=config_base \
+	KERNEL_CONFIGURE=no \
+	RELEASE=jammy \
+	KERNEL_GIT=shallow \
+	EXPERT="yes" \
+	CLEAN_LEVEL=oldcache \
+	\
+	PACKAGE_LIST_BOARD="\
+	cmake libi2c-dev \
+	gdb git gcc net-tools rfkill bluetooth bluez bluez-tools blueman \
+	logrotate python3-pip mergerfs inotify-tools python3-dbus dnsmasq-base \
+	python3-dev python-is-python3 python3-pip python3-gi python3-gi-cairo gir1.2-gtk-3.0 dnsmasq-base lshw  \
+	debhelper build-essential ntfs-3g fakeroot lockfile-progs \
+	libip6tc2 libnftnl11 iptables \
+	" \
+	#usbmount: ebhelper build-essential ntfs-3g fakeroot lockfile-progs
+	#docker:   libip6tc2 libnftnl11 iptables
+	#fula: logrotate
+
+} # ArmbianCompileDesktop
 ########################################################
 DockerOffline()
 {
 	docker save functionland/node:release -o $DOCKER_OFFLINE_PATH/node_release.tar
 	docker save functionland/go-fula:release -o $DOCKER_OFFLINE_PATH/go_fula_release.tar
 	docker save functionland/fxsupport:release -o $DOCKER_OFFLINE_PATH/fxsupport_release.tar
+
 } # DockerOffline
 ########################################################
 
